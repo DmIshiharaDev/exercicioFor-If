cd72481b6d2722acf814b51c7ef7a933e2baa0db
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-20 13:49:54 -0300

    Adicionar arquivos de projeto.

---
+++ exercicioFor-If/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mail;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int senha = 2002;
+
+        while (true)
+        {
+            Console.WriteLine("Digite sua senha: ");
+            int senha1 = int.Parse(Console.ReadLine());
+
+            if (senha1 != senha) 
+            {
+                Console.WriteLine("Senha inválida");
+                Console.WriteLine();
+            }
+            else {
+                Console.WriteLine("Senha aceita com sucesso");
+                break;
+
+                    }
+        }
+    }
+}
